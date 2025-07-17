bf39e47624bd779d45e4f1be38087016f8f64de3
QuanTT <thaithienquan09@gmail.com>
2025-07-17 23:52:09 +0700

    Demo todolist layout

---
+++ src/Demo/todoList.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export default function Todolist(){
+    const [inputValue, setinputValue] = useState('');
+    const[task, settask]=useState('');
+    return(<></>)
+}
