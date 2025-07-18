e29f4dd5085bc6f523eb46864eacc4d2333041e6
QuanTT <thaithienquan09@gmail.com>
2025-07-18 23:34:48 +0700

    Comleted  todolist lv1

--- src/Demo/todoList.tsx
+++ src/Demo/todoList.tsx
@@ -1,7 +1,38 @@
 import { useState } from "react";
 
-export default function Todolist(){
-    const [inputValue, setinputValue] = useState('');
-    const[task, settask]=useState('');
-    return(<></>)
+export default function Todolist() {
+  const [inputValue, setinputValue] = useState("");
+  const [task, settask] = useState(["fbhedkjfb", "èhudshf"]);
+  const handleChange = (e) => {
+    setinputValue(e.target.value);
+  };
+  const addTodo = () => {
+    settask([...task, inputValue.trim()]);
+    setinputValue("");
+  };
+  const deleteTask = (i) => {
+   const newtodo= task.filter((_, it) => it !== i);
+    settask(newtodo);
+  };
+  return (
+    <>
+      <div className="container mt-5">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputValue}
+          placeholder="Add task"
+        />
+        <button onClick={addTodo}>Add todo</button>
+      </div>
+      <ul>
+        {task.map((task, i) => (
+          <li key={i}>
+            {task}
+            <button onClick={() => deleteTask(i)}></button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
