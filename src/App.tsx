e6c7328936de29338e2f314b3a1a460f84511d9d
QuanTT <thaithienquan09@gmail.com>
2025-07-28 23:07:36 +0700

    update  todolist with useContext v2.02

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Todo } from "./types/todo";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import { v4 as uuidv4 } from "uuid";
-
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
